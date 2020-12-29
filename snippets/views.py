f57fa61e9f3a21c30b208e0270a807256ee36cb5
Jaguso <jagutzo@gmail.com>
2020-12-28 19:59:59 -0600

    Added user model, serializer, view and url

--- snippets/views.py
+++ snippets/views.py
@@ -3,12 +3,14 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import status, mixins, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from django.contrib.auth.models import User
 
 
 #Usamos csfr_exempt para que el usuario no necesite token al hacer este request
@@ -198,3 +200,14 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
