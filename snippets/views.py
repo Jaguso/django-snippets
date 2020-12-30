b9221b4f90c8c2cba1bf17fb9e3e77f2cd5f1a96
Jaguso <jagutzo@gmail.com>
2020-12-29 19:59:00 -0600

    Added authentication

--- snippets/views.py
+++ snippets/views.py
@@ -6,11 +6,12 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
-from rest_framework import status, mixins, generics
+from rest_framework import status, mixins, generics, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 #Usamos csfr_exempt para que el usuario no necesite token al hacer este request
@@ -196,11 +197,16 @@ from django.contrib.auth.models import User
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
