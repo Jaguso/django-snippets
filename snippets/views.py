eba11e81e3a65c78a6397f27a872d9484c52d8d0
Jaguso <jagutzo@gmail.com>
2021-01-15 21:16:57 -0600

    Added hyperlinking and pagination

--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,8 @@ from rest_framework.views import APIView
 from rest_framework import status, mixins, generics, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
@@ -62,7 +64,7 @@ from snippets.permissions import IsOwnerOrReadOnly
 #         snippet.delete()
 #         return HttpResponse(status=204)
 
-########################
+###################################
 
 # # Refactor de la función de arriba
 # @api_view(['GET', 'POST'])
@@ -215,5 +217,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
