ede2c27f0ae8ea747bcc163df41d0c75ddfdb2e7
Jaguso <jagutzo@gmail.com>
2021-01-19 23:53:08 -0600

    b

--- authors/models.py
+++ authors/models.py
@@ -5,6 +5,7 @@ class Authors(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, default='')
     nationality = models.CharField(max_length=50, default='')
+    gender = models.CharField(max_length=50, default='male')
 
     class Meta: 
         ordering = ['created']
